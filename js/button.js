806740adcba6e50d377a718f40dfae18fe636e7b
Kristin Sellers <kristinmsellers@gmail.com>
2018-04-28 10:29:52 -0700

    Update button.js

--- js/button.js
+++ js/button.js
@@ -2,13 +2,3 @@ function changeTextZoom() {
 document.getElementById("myButton").innerHTML = "RGIS Comments";
 		//let's zoom all the way out
 map.fitBounds((bounds));}
-
-
-//function changeTextZoom() {
-//    document.getElementById("myButton").innerHTML = "Zoom to World";
-		//let's zoom all the way out
-//		  map.fitBounds([
-//        [40.712, -74.227],
-//        [40.774, -74.125]
-//		    ]);
-//}
